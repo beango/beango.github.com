48026e5999348ae16baaf2ad8dfe6723bdbab281
beango <6588617@gmail.com>
2013-06-27 15:11:24 +0800

    Signed-off-by: beango <6588617@gmail.com>

--- stage/assets/default.js
+++ stage/assets/default.js
@@ -1,6 +1,17 @@
+/*! default.js */
+function toggle(obj){
+	var tobj = $(obj).parent().next();
+	tobj.toggle(0,function(){
+		if(tobj.css("display")=="none")
+			$(obj).text("+ 点击展开");
+		else
+			$(obj).text("- 点击收起");
+	});
+	event.preventDefault();
+}
 
 $(function() {
-  $('pre').addClass('prettyprint linenums').attr('style', 'overflow:auto');
-  window.prettyPrint && prettyPrint();
-  $(".linkcodetoggle").parent().next().hide();
+    $('pre').addClass('prettyprint linenums').attr('style', 'overflow:auto');
+    window.prettyPrint && prettyPrint();
+    $(".linkcodetoggle").parent().next().hide();
 });
