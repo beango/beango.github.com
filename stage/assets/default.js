1b34d3b5092ad87caa535a10d007c9b9662b8eba
beango <6588617@gmail.com>
2013-06-29 14:48:49 +0800

    Signed-off-by: beango <6588617@gmail.com>

--- stage/assets/default.js
+++ stage/assets/default.js
@@ -14,4 +14,6 @@ $(function() {
     $('pre').addClass('prettyprint linenums').attr('style', 'overflow:auto');
     window.prettyPrint && prettyPrint();
     $(".linkcodetoggle").parent().next().hide();
+    $("#wordcloud").tagcloud({colormin:"d88",colormax:"0a0"});
+    $("#wordcloud-left").tagcloud({type:"list",sizemin:8,colormin:"d88",colormax:"0a0"});
 });
